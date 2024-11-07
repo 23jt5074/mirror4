1f9996c6e7e95e5179e1a9fa11ef8a7662c5cb13
23jt5074 <23jt5074@barton.ac.uk>
2024-11-07 18:20:09 +0000

    edited html

--- functions/proxy.js
+++ functions/proxy.js
@@ -2,29 +2,36 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  const { url } = JSON.parse(event.body);
-  if (!url) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "No URL provided" }),
-    };
-  }
+    console.log("Function called with event:", event);
 
-  try {
-    const response = await fetch(url);
-    const contentType = response.headers.get("content-type");
+    try {
+        const { url } = JSON.parse(event.body);
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": contentType,
-      },
-      body: await response.text(),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+        if (!url) {
+            console.error("No URL provided");
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "No URL provided" }),
+            };
+        }
+
+        console.log("Fetching URL:", url);
+        const response = await fetch(url);
+        const contentType = response.headers.get("content-type");
+
+        console.log("Fetched data with content-type:", contentType);
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": contentType,
+            },
+            body: await response.text(),
+        };
+    } catch (error) {
+        console.error("Error fetching URL:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };
